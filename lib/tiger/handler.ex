16a83da198632316379c128705e0e42a0be7e82a
Dino Maric <dino.onex@gmail.com>
2019-05-17 09:54:41 +0200

    Add response status codes

--- lib/tiger/handler.ex
+++ lib/tiger/handler.ex
@@ -13,21 +13,31 @@ defmodule Tiger.Handler do
       |> List.first
       |> String.split(" ")
 
-    %{ method: method, path: path, body: "" }
+    %{ method: method, path: path, status: nil, body: "" }
   end
 
   def route(conv) do
-    %{ conv | body: "Tiger, tiger!" }
+    %{ conv | status: 200, body: "Tiger, tiger!" }
   end
 
   def format_response(conv) do
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
     Content-Lenght: #{String.length(conv.body)}
 
     #{conv.body}
     """
+  end
 
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
   end
 end
